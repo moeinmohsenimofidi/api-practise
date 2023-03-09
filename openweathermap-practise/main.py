bce25d6784f4cfaab18de19deaa451654460fc4a
Moein <moien.mmofidi88@gmail.com>
2023-03-09 11:12:21 +0100

    add new py file

--- openweathermap-practise/main.py
+++ openweathermap-practise/main.py
@@ -26,44 +26,69 @@ city_list = ["Graun im vinschgau","Maso corto","Mals","stilfs","Laas","Schlander
 '''
 
 # test cities
-test_cities = ["Bolzano", "Merano"]
+tesCities = ["Bolzano", "Merano"]
 
 
-API_KEY = "702aaee7581400652b3e4dca17874e95"
+openweatherKey = "3781a117fbc24b712b262512ef3e1139"
 GEOCODE_ENDPOINT = "http://api.openweathermap.org/geo/1.0/direct"
-#history_endpoint = f"http://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&units=metric&dt={start_epoch}&appid={API_KEY}"
+HISTORIC_URL = f"http://api.openweathermap.org/data/3.0/onecall/timemachine"
 
 
 # script for find latitude and longitude for all location in the map
 
+def epoch_time():
+    return int(time.time() // 86400) * 86400 - 86400
 
-def find_location (cities):
+
+def find_location(cities):
     locations = []
     for city in cities:
-        geocode_parameters = {
-            "q": f"{city},IT",
-            "appid": API_KEY
-        }
-        geo_response = requests.get(GEOCODE_ENDPOINT, params=geocode_parameters).json()
+        geo_parameters = {"q": f"{city},IT", "appid": openweatherKey}
+        geo_response = requests.get(GEOCODE_ENDPOINT, params=geo_parameters).json()
         for item in geo_response:
             lat_lon = {
                 "city": item["name"],
                 "lat": item["lat"],
                 "lon": item["lon"]
-            }
-        locations.append(lat_lon)
+                }
+            locations.append(lat_lon)
     return locations
 
-cities_location = find_location(test_cities)
-base_time = (int(time.time() // 86400)) * 86400 - 86400
-weather_data = []
-for city in cities_location:
-    print(city)
-    lat = city["lat"]
-    lon = city["lon"]
-    while base_time <= 1677975000:
-        print("#")
-        history_url = f"http://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&units=metric&dt={epoch_time}&appid={API_KEY}"
-        history_response = requests.get(history_url).json()
-        weather_data.append(history_response)
-        base_time += 600
+
+
+def get_historic_data(cities_location):
+    epochTime = epoch_time()
+    historic_data = []
+    for city in cities_location:
+        baseTime = epochTime
+        while baseTime <= 1678061400:
+            parameters = {
+                "lat": city["lat"],
+                "lon": city["lon"],
+                "dt": baseTime,
+                "lang": "en",
+                "units": "metric",
+                "appid": openweatherKey,
+            }
+            history_response = requests.get(HISTORIC_URL, params=parameters).json()
+            historic_data.append(history_response)
+            baseTime += 600
+
+    return historic_data
+
+print(epoch_time())
+
+
+data = find_location(tesCities)
+print(data)
+
+with open('historic_data', 'w') as file:
+    json.dump(data, file)
+
+
+'''
+dt = epoch_time()
+iso_time = datetime.utcfromtimestamp(dt).isoformat()
+print(iso_time)
+
+'''
